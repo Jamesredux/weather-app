78315e1a038065aad8bf4bacf59170a3ce5f5b3a
James McCann <jamesredux@gmail.com>
2021-08-24 18:05:09 +0700

    styled current weather

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -23,45 +23,51 @@ class WeatherData extends Component {
     return (
       <div className='weather-current'>
         <div className='city-name'>
-          <div className='large'>{city}</div>
+          <div className='large city-box'>{city},</div>
           <div className='large'>{country}</div>
         </div>
-        <div className='current-top'>
-          <div className='current-time'>{dt}</div>
+        <div className='date-box'>
+          <div className='current-time yellow'>{dt}</div>
+        </div>
+        <div className='main-info-box'>
           <div className='current-temp'>
             {temp}
-            {this.props.units === 'metric' ? <span> C</span> : <span> F</span>}
+            {this.props.units === 'metric' ? (
+              <span>&#xb0;C</span>
+            ) : (
+              <span>&#xb0;F</span>
+            )}
           </div>
-          <div className='feels-like'>
-            Feels Like: {feels_like}
-            {this.props.units === 'metric' ? <span> C</span> : <span> F</span>}
+          <div className='details-icon'>
+            <img
+              src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+              height='100'
+              width='100'
+              alt='icon'
+            />
           </div>
         </div>
-        <div className='current-extra'>
-          <div className='current-details'>
-            <div className='details-box'>
-              <div className='details-text'>
-                <div className='details-main'>{main}</div>
-                <div className='details-extra'>{details}</div>
-              </div>
-              <div className='details-icon'>
-                <img
-                  src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-                  height='100'
-                  width='100'
-                  alt='icon'
-                />
-              </div>
-            </div>
-
-            <div className='uvi-humid'>
-              <div className='uvi'>UVI: {uvi}</div>
-              <div className='humidity'>Humidity: {humidity}</div>
-            </div>
-            <div className='sun-updown'>
-              <div className='sunrise'>Sunrise: {sunrise}</div>
-              <div className='sunset'>Sunset: {sunset}</div>
-            </div>
+        <div className='extra-box-1'>
+          <div className='details-extra'>{details}</div>
+          <div className='feels-like'>
+            Feels Like: {feels_like}
+            {this.props.units === 'metric' ? (
+              <span>&#xb0;C</span>
+            ) : (
+              <span>&#xb0;F</span>
+            )}
+          </div>
+        </div>
+        <div className='extra-box-2'>
+          <div className='uvi'>UVI: {uvi}</div>
+          <div className='humidity'>Humidity: {humidity}</div>
+        </div>
+        <div className='sunrise-sunset'>
+          <div className='sunrise'>
+            Sunrise: <span className='yellow'>{sunrise}</span>
+          </div>
+          <div className='sunset'>
+            Sunset: <span className='yellow'>{sunset}</span>
           </div>
         </div>
       </div>
